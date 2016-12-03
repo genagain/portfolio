c8ffc5ad77f14a991d678b97abcaae455cc7b06c
genagain <ohta.g@husky.neu.edu>
2016-12-03 02:03:10 -0500

    wip commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,15 @@
 import framework from 'framework'
 import domselect from 'dom-select'
 import utils from 'utils'
+import queryDom from 'query-dom-components'
+import config from 'config'
+import events from 'dom-event'
 
 class App {
 
   constructor(opt = {}) {
+
+    this.dom = queryDom({ el: config.body })
 
     this.init()
   }
@@ -18,7 +23,20 @@ class App {
   addEvents() {
 
     utils.biggie.addRoutingEL(domselect.all('nav a'))
+    // add submit event listener
+
+    events.on(this.dom.submit, 'submit', this.onSubmit, this)
+
+    // check the value of the input once the submit event lister is trigger
+    // map the value to a route
   }
+
+  onSubmit(e) {
+
+    console.log('submit')
+    return false
+  }
+
 }
 
 module.exports = App
