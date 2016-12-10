03138b7be414612bd675b36abe0748aa13a28f14
mikehwagz <hmiklw@me.com>
2016-12-09 19:24:27 -0500

    add commandHistory as empty array to config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,16 +1,18 @@
 import domselect from 'dom-select'
 
 const config = {
-	
+
 	BASE: '/',
-	
+
 	body: document.body,
 	view: domselect('main'),
 
 	width: window.innerWidth,
 	height: window.innerHeight,
-    
-    infos: null
+
+  infos: null,
+
+	commandHistory: []
 }
 
 export default config
