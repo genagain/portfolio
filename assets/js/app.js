8c3ade6d5e09b65b3c94891d655a55a4e9d35250
mikehwagz <hmiklw@me.com>
2016-12-13 03:33:19 -0500

    add keyPress event listener and handler

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,6 +12,7 @@ class App {
     this.dom = queryDom({ el: config.body })
 
     this.onSubmit = this.onSubmit.bind(this)
+    this.onKeyPress = this.onKeyPress.bind(this)
 
     this.init()
   }
@@ -30,6 +31,28 @@ class App {
 
     // listen to submit event
     events.on(this.dom.form, 'submit', this.onSubmit)
+    events.on(this.dom.input, 'keydown', this.onKeyPress)
+  }
+
+  onKeyPress(e) {
+
+  switch(e.keyCode) {
+      case App.KEY_UP:
+        if (this.index > 0 && this.index <= config.hist.length) {
+          this.index -= 1
+          this.dom.input.value = config.hist[this.index]
+        }
+        break
+
+      case App.KEY_DOWN:
+        if (this.index >= 0 && this.index < config.hist.length) {
+          this.index += 1
+          this.dom.input.value = this.index === config.hist.length ? '' : this.dom.input.value = config.hist[this.index]
+        }
+        break
+
+      default: break
+    }
   }
 
   onSubmit(e) {
@@ -37,7 +60,9 @@ class App {
     e.preventDefault()
 
     const userInput = this.dom.input.value.trim()
-    userInput !== '' ? config.commandHistory.push(userInput) : null
+    userInput !== '' ? config.hist.push(userInput) : null
+
+    this.index = config.hist.length
 
     const { commands } = window._data
 
@@ -52,7 +77,7 @@ class App {
       this.printCommandResponseList(commands.projects.list)
 
     } else if (userInput === 'clear') {
-      this.dom.commandHistory.innerHTML = ''
+      this.dom.commands.innerHTML = ''
 
     } else if (userInput === '') {
       // add blank segment logic here
@@ -65,20 +90,23 @@ class App {
   }
 
   printCommandResponse(response) {
-
     const li = document.createElement('li')
-
     li.innerHTML = response
-
-    this.dom.commandHistory.appendChild(li)
+    this.dom.commands.appendChild(li)
   }
 
   printCommandResponseList(arr) {
-
     arr.forEach(x => {
-
       this.printCommandResponse(x)
     })
+  }
+
+  static get KEY_UP() {
+    return 38
+  }
+
+  static get KEY_DOWN() {
+    return 40
   }
 }
 
