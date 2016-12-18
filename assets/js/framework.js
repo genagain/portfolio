6afeb8fd24997c3557acd7d5c9b72b09109702f6
mikehwagz <hmiklw@me.com>
2016-12-18 11:40:46 -0500

    remove comments

--- assets/js/framework.js
+++ assets/js/framework.js
@@ -1,14 +1,8 @@
 import bigwheel from 'bigwheel'
 
-/* ----------
-create our framework instance
-see https://github.com/bigwheel-framework/documentation/blob/master/quickstart.md#bigwheel-quick-start
----------- */
 module.exports = bigwheel((done) => {
 	done({
-		// https://github.com/bigwheel-framework/documentation/blob/master/misc.md#overlap
 		overlap: true,
-		// https://github.com/bigwheel-framework/documentation/blob/master/routes-special.md#initsection
 		initSection: require('./sections/preloader'),
 		routes: require('./routes')
 	})
--- assets/js/routes.js
+++ assets/js/routes.js
@@ -1,9 +1,5 @@
 import config from 'config'
 
-/* ----------
-all routes needs to be defined inline
-see https://github.com/bigwheel-framework/documentation/blob/master/routes-defining.md#as-section-standard-form
----------- */
 module.exports = {
 	[`${config.BASE}`]: require('./sections/home'),
 	[`${config.BASE}home`]: { section: require('./sections/home') },
