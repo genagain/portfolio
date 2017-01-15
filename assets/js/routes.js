c7c1de0ce0149856f51bca1e24b8584fd2cd3300
mikehwagz <hmiklw@me.com>
2017-01-15 00:16:25 -0500

    add 404 redirect route

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -3,5 +3,6 @@ import config from 'config'
 module.exports = {
 	[`${config.BASE}`]: require('./sections/home'),
 	[`${config.BASE}home`]: { section: require('./sections/home') },
-	[`${config.BASE}project/:id`]: { section: require('./sections/section'), duplicate: true }
+	[`${config.BASE}project/:id`]: { section: require('./sections/section'), duplicate: true },
+	'404': `${config.BASE}`
 }
